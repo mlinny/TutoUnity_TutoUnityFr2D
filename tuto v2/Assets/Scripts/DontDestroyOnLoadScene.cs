575b26ab87719a62828d5e56d391e134623b8373
mlinny <maitrelin@yahoo.fr>
2020-10-21 23:49:21 +0200

    Tuto #9

---
+++ tuto v2/Assets/Scripts/DontDestroyOnLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DontDestroyOnLoadScene : MonoBehaviour
+{
+
+    public GameObject[] objets;
+
+    private void Awake()
+    {
+        foreach(var element in objets)
+        {
+            DontDestroyOnLoad(element);
+        }
+    }
+}
---
+++ tuto v2/Assets/Scripts/LoadSpecificScene.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+
+    public string SceneName;
+    private  Animator fadeSystem;
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    public IEnumerator LoadNextScene()
+    {
+        fadeSystem.SetTrigger("FadeIn"); 
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneName);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(LoadNextScene());
+            
+        }
+    }
+}
